7aa4a2d06b648994e5bb9c944f787901ac1a650c
HarveyMalapit <45374730+Malapitharvey@users.noreply.github.com>
2018-11-27 08:15:01 +0800

    Create Dynamic_Input.py

---
+++ Dynamic_Input.py
@@ -0,0 +1,9 @@
+name = input("enter you name: ")
+print ("your name is:" ,name)
+
+Hnum = int(input("enter the number: "))
+Cnum = int(input("enter the number: "))
+
+raises = int("10")
+print ("The sum is: " , Hnum + Cnum)
+print ("5 raises to 5: "  , raises ** 5)
