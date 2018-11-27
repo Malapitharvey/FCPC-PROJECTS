2b8973b7c5aab1011fd2e02ef756230073f6660c
HarveyMalapit <45374730+Malapitharvey@users.noreply.github.com>
2018-11-27 10:00:15 +0800

    Create List_Malapit.py

---
+++ List_Malapit.py
@@ -0,0 +1,30 @@
+strings_V = ["cat","dog","rat"]
+char_V = ['h','a','r','v','e','y']
+int_V = [1,2,3,4,5]
+float_V = [1.1,2.2,3.3,4.4,5.5]
+
+print("Strings")
+for x in range(len(strings_V)):
+    print(strings_V[x])
+print()
+
+print("Character")
+for x in range(len(char_V)):
+    print(char_V[x])
+print()
+
+print("Integers")
+for x in range(len(int_V)):
+    print(int_V[x])
+print()
+
+print("Float")
+for x in range(len(float_V)):
+    print(float_V[x])
+print()
+
+print("Len")
+print(len(strings_V))
+print(len(char_V))
+print(len(int_V))
+print(len(float_V))
